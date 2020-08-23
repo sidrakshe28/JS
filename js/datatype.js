42e6e1e9111c7916eb79148271b80a1778ac2294
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-08-23 15:07:46 +0530

    added primitive data types

---
+++ js/datatype.js
@@ -0,0 +1,22 @@
+ //primitive data types
+
+ //string
+ var name="sid";
+ console.log("my string is" + name);
+ console.log("Data type is" + (typeof name ));
+
+ //numbers
+ let marks = 34;
+ console.log('data tupe is'+ (typeof marks));
+
+ //Boolean
+ //Let isDriver = true;
+ //console.log("data type is" + (typeof isDriver));
+
+ //null
+let nullval = null;
+console.log("data type is" + (typeof nullvar));
+
+//undefined
+let undef=undefined;
+console.log("data type is" +(typeof undef));
