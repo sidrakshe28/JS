d2c6ce95342b20d23761ea9508d3aa78ec25b79b
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-08-23 13:25:56 +0530

    first commit

---
+++ js/stringtut.js
@@ -0,0 +1,12 @@
+console.log('we are at tutorial 6');
+const name='siddhika';
+const greeting='good morning';
+console.log(greeting +" "+Name);
+
+let html;
+html="<h1> this is heading </h1>"+
+     "<p> this is my para </p>";
+
+html=html.concat('this','str2');
+console.log(html);
+     
