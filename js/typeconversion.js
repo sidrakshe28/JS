dbdee6ea1ac0f4343649a4c2fdf82cfe9ee9b8f7
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-08-23 19:37:54 +0530

    type conversion and coercion

--- js/typeconversion.js
+++ js/typeconversion.js
@@ -13,5 +13,13 @@ stri =Number("343d4");
 stri=Number(false);
 stri= Number([1,2,3,4,5,6,8,9]);
 //console.log(stri,  (typeof stri));
+
+
 let number= parseFloat('34.098');
 console.log(number.toFixed(2), (typeof number));
+
+//type coercion
+
+let mystr= Number("698");
+let mynum=34;
+console.log(mystr+mynum);
