330b42fe40b829015a44f5aedb5ce2385ba1bcff
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-08-28 13:03:24 +0530

    loops

---
+++ js/loops.js
@@ -0,0 +1,20 @@
+console.log('loops tut');
+
+ //let i=0
+//for(i=0;i<100;i++){
+  //  console.log(i);
+//}
+
+//let j=0;
+//while(j<100){
+  //  console.log(j);
+    //j=j+1;
+
+//}
+
+let k=110;
+do{
+    console.log(k+1);
+    k=k+1;
+
+}while(k <10);
