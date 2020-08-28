bb17ec3a18d70cd529c2c8ffbd0774765b6db7b6
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-08-28 12:40:10 +0530

    if else condition & swutches

---
+++ js/ifelseswitch.js
@@ -0,0 +1,25 @@
+console.log('this is tutorial on if-else ad swicth statement ');
+const age=19;
+//if(age==19){
+  //  console.log('age is 19');
+
+//}
+//else if(age==65){
+  //  console.log('age is 65');
+
+//}
+//else{
+  //  console.log('age is 21');
+//}
+
+switch(age){
+ case 18:
+     console.log("you ar 80");
+     break;
+  case 28:
+      console.log("you are 18");
+   default:
+       console.log("you are 18");
+       break;
+
+}
